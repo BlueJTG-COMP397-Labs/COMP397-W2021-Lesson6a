0bc9ed3bbf38e21736053bdc89ee6a87626d3f38
Timothy Garcia <timothyiangarcia@gmail.com>
2021-02-27 22:59:03 -0500

    Added enemy animations

---
+++ Assets/_Scripts/CryptoBehaviour.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum CryptoState
+{
+    IDLE,
+    RUN,
+    JUMP
+}
+public class CryptoBehaviour : MonoBehaviour
+{
+    [Header("Line of Sight")]
+    public bool HasLOS;
+    public GameObject player;
+
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HasLOS)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+        
+        
+        if (HasLOS && Vector3.Distance(transform.position, player.transform.position) < 2.5)
+        {
+            //could be attack function
+            animator.SetInteger("AnimState", (int)CryptoState.IDLE);
+            transform.LookAt(transform.position - player.transform.forward);
+            if (agent.isOnOffMeshLink)
+            {
+                animator.SetInteger("AnimState", (int)CryptoState.JUMP);
+            }
+        }
+        else
+        {
+            animator.SetInteger("AnimState", (int)CryptoState.RUN);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log(other.gameObject.name + " has entered enemy LOS");
+            HasLOS = true;
+            player = other.transform.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log(other.gameObject.name + " is out of enemy LOS");
+            HasLOS = false;
+        }
+    }
+}
